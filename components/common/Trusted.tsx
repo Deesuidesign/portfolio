2867b900b54173aa7a32d24ca69f14f790829ec8
Deesuidesign <deesuidesign@gmail.com>
2024-09-11 18:12:22 +0100

    Update Trusted.tsx

--- components/common/Trusted.tsx
+++ components/common/Trusted.tsx
@@ -1,6 +1,6 @@
 // components/common/Trusted.js
 import Image from "next/image";
-import BaoBad Plus from "../../public/zoom.svg";
+import BaoBadPlus from "../../public/zoom.svg";
 import Spotify from "../../public/sportify.svg";
 import Zoom from "../../public/zoom.svg";
 import Slack from "../../public/slack.svg";
@@ -9,7 +9,7 @@ import Adobe from "../../public/adobe.svg";
 
 // Array of company logos
 const companies = [
-  { name: "BaoBad Plus", logo: BaoBad Plus },
+  { name: "BaoBadPlus", logo: BaoBadPlus },
   { name: "Spotify", logo: Spotify },
   { name: "Zoom", logo: Zoom },
   { name: "Slack", logo: Slack },
