2c2f9d9458fa481b797c7b6f77f08a5b3922cb21
Deesuidesign <deesuidesign@gmail.com>
2025-01-21 10:22:34 +0100

    Update Trusted.tsx

--- components/common/Trusted.tsx
+++ components/common/Trusted.tsx
@@ -9,7 +9,7 @@ const companies = [
   { name: "Simpu", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1726056453/mcabmdfedbovaemldkzo.svg" },
   { name: "FlowWave Inc", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1726056451/n4jr30qmompcmywolfla.svg" },
   { name: "Spark Xplorer", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1726056455/ucupwtrkswhk3yiligvi.svg" },
-  { name: "1401 Apartment", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1726056449/t18vxcz2dohrhsq0qd2w.svg" },
+  { name: "Tri Edge Technologies", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1737451301/Tri-Edge_Technologies_Logotrt_ev1avq.png" },
   { name: "Airbnb", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1727946906/superhost_id_cardrere_fpnadq.png" },
   { name: "Riverdean", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1726056452/bvhbfbilhh8ufthx2gma.svg" },
   { name: "SB Telecoms", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1726056453/y1j3ljfisbquayj4r2z9.svg" },
