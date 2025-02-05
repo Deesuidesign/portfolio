9c09b48eef4649c3f7942c1aa989d77412cf904f
Deesuidesign <deesuidesign@gmail.com>
2025-02-05 22:16:59 +0100

    Update UIUX.tsx

--- components/common/UIUX.tsx
+++ components/common/UIUX.tsx
@@ -21,7 +21,7 @@ import "swiper/css/navigation";
 // Array of project data
 const projects = [
   {
-    title: "Pews Pro Church Management Software",
+    title: "Pews-Pro Church Management Software",
     status: "Development Stage",
     image:
       "https://res.cloudinary.com/delbjwwfy/image/upload/v1737529379/Captureytytytre_r_t8sozq.png",
@@ -48,58 +48,6 @@ const projects = [
       },
       {
         title: "Project Screens",
-        content: [
-          {
-            type: "swipper",
-            img_list: [
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1737529379/Captureytytytre_r_t8sozq.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1737529379/Captureytytytre_r_t8sozq.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1737529379/Captureytytytre_r_t8sozq.png",
-            ],
-          },
-        ],
-      },
-    ],
-    {
-    title: "Xplorer EdTech Platform Design",
-    status: "Live",
-    image:
-      "https://res.cloudinary.com/delbjwwfy/image/upload/v1737530305/Captureyrrtrt_pnhqyd.png",
-    link: 
-      "https://www.sparkxplorer.com/",
-    content: [
-      {
-        title: "Project Overview",
-        content: [
-          {
-            type: "text",
-            text: "Pews Pro Church Management Software is an end-to-end software solution that focuses heavily on automating key church activities such as managing church events, coordinating any church payments seamlessly, and keeping a standard database for the congregation.",
-          },
-          {
-            type: "text",
-            text: "Another Paragraph",
-          },
-          {
-            type: "text",
-            text: "Pews Pro Church Management Software is an end-to-end software solution that focuses heavily on automating key church activities such as managing church events, coordinating any church payments seamlessly, and keeping a standard database for the congregation.",
-          },
-        ],
-      },
-      {
-        title: "Section 2",
-        content: [
-          {
-            type: "video",
-            url: "https://www.youtube.com/embed/HAdROFr7VRQ?si=DgbZT9WxrDFY5KMJ",
-          },
-          {
-            type: "text",
-            text: "Another Paragraph",
-          },
-        ],
-      },
-      {
-        title: "Section 3 - Project Screens",
         content: [
           {
             type: "swipper",
