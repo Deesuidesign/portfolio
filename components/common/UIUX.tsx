fbd2530be89a31688d0681db37ba99ad69a4b5be
Deesuidesign <deesuidesign@gmail.com>
2025-02-06 13:13:50 +0100

    Update UIUX.tsx

--- components/common/UIUX.tsx
+++ components/common/UIUX.tsx
@@ -68,7 +68,6 @@ const projects = [
     status: "Development Stage",
     image:
       "https://res.cloudinary.com/delbjwwfy/image/upload/v1738840462/Captureyrrtrt_pnhqyd_mbhtao.png",
-    link: "https://www.sparkxplorer.com/",
     content: [
       {
         title: "Project Overview",
