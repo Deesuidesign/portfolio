b0de29540ac2ee73081759d40dcd087fd1254911
Deesuidesign <deesuidesign@gmail.com>
2025-02-14 15:08:51 +0100

    Update Trusted.tsx

--- components/common/Trusted.tsx
+++ components/common/Trusted.tsx
@@ -11,7 +11,7 @@ const companies = [
   { name: "Spark Xplorer", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1726056455/ucupwtrkswhk3yiligvi.svg" },
   { name: "Tri Edge Technologies", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1737453334/Tri-Edge_Technologies_Logotrt_uo3o0l.png" },
   { name: "Airbnb", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1727946906/superhost_id_cardrere_fpnadq.png" },
-  { name: "Riverdean", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1726056452/bvhbfbilhh8ufthx2gma.svg" },
+  { name: "Podium App", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1739542076/Podium_Betting_App_logo_qo3pfp.svg" },
   { name: "SB Telecoms", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1726056453/y1j3ljfisbquayj4r2z9.svg" },
   { name: "Living", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1726056451/cvbojlojfuqygoqrrc8v.svg" },
 ];
