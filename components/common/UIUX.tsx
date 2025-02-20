8bb52cc5f770136b8b2976680fe936f471b45bf1
Deesuidesign <deesuidesign@gmail.com>
2025-02-20 09:36:06 +0100

    Update UIUX.tsx

--- components/common/UIUX.tsx
+++ components/common/UIUX.tsx
@@ -49,7 +49,20 @@ const projects = [
             text: "SparkXplorer was created to bridge this gap by offering an interactive, affordable, and accessible digital learning experience.",
           }
         ],
-      },      
+      },
+      {
+        title: "Solution Statement",
+        content: [
+          {
+            type: "text",
+            text: "To address the challenges faced by working families and students, the design focuses on delivering an intuitive, engaging, and accessible e-learning experience.",
+          },
+          {
+            type: "text",
+            text: "Through an empathetic and research-driven design approach, SparkXplorer enhances accessibility, fosters engagement, and empowers families in their educational journey, ensuring a more equitable and effective after-school learning experience.",
+          }
+        ],
+      },                  
      {
         title: "Project Prototype",
         content: [
@@ -79,19 +92,6 @@ const projects = [
           },
         ],
       },
-      {
-        title: "Solution Statement",
-        content: [
-          {
-            type: "text",
-            text: "To address the challenges faced by working families and students, the design focuses on delivering an intuitive, engaging, and accessible e-learning experience.",
-          },
-          {
-            type: "text",
-            text: "Through an empathetic and research-driven design approach, SparkXplorer enhances accessibility, fosters engagement, and empowers families in their educational journey, ensuring a more equitable and effective after-school learning experience.",
-          }
-        ],
-      },            
     ],
     description: [
       "Pews Pro Church Management Software is an end-to-end software solution that focuses heavily on automating key church activities such as managing church events, coordinating any church payments seamlessly, and keeping a standard database for the congregation.",
