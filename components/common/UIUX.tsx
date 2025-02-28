ddbf50398cee61c8bec5843d0ca8bfb81c0b02c3
Deesuidesign <deesuidesign@gmail.com>
2025-02-28 15:51:51 +0100

    Update UIUX.tsx

--- components/common/UIUX.tsx
+++ components/common/UIUX.tsx
@@ -158,14 +158,16 @@ const projects = [
           {
             type: "swipper",
             img_list: [
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833832/Hugley_5_hkaat7.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833833/Hugley_2_drmt4r.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833833/Hugley_3_aicwtg.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833874/Hugley_90_v93hik.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833836/Hugley_4_ietcvt.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833831/Hugley_6_tiorxt.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833831/Hugley_8_jlwhrg.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833831/Hugley_7_ul3pfh.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1740753890/PaySphere_1_yhsaon.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1740753891/PaySphere_2_uhywcd.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1740753891/PaySphere_3_ld4srv.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1740753891/PaySphere_4_kotb5a.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1740753891/PaySphere_5_q0sp4u.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1740753891/PaySphere_6_qyn2oz.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1740753892/PaySphere_7_d6bbm9.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1740753892/PaySphere_8_vvnr8h.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1740753894/PaySphere_9_advhv6.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1740753894/PaySphere_10_bi89vs.png",             
             ],
           },
         ],
@@ -175,76 +177,7 @@ const projects = [
       "Pews Pro Church Management Software is an end-to-end software solution that focuses heavily on automating key church activities such as managing church events, coordinating any church payments seamlessly, and keeping a standard database for the congregation.",
       "It aims at relieving church pastors and administrative workers of the huge burden of keeping track of members, finances, and all other important records.",
     ],
-  },  
-  {
-    title: "Hugley Dating App Design",
-    status: "Development Stage",
-    image:
-      "https://res.cloudinary.com/delbjwwfy/image/upload/v1738840462/Captureyrrtrt_pnhqyd_mbhtao.png",
-    content: [
-      {
-        title: "Project Overview",
-        content: [
-          {
-            type: "text",
-            text: "Hugley is a unique dating app specifically for individuals with intellectual disabilities, designed to foster meaningful connections in a safe and inclusive environment.",
-          },
-          {
-            type: "text",
-            text: "Whether you're seeking friendship, companionship, or love, Hugley provides a supportive space where users can interact comfortably and authentically.",
-          }                      
-        ],
-      },
-      {
-        title: "Problem Statement",
-        content: [
-          {
-            type: "text",
-            text: "Individuals with intellectual disabilities often encounter barriers when using mainstream dating apps due to complex interfaces, safety concerns, and social stigmas.",
-          },
-          {
-            type: "text",
-            text: "There is a lack of platforms catering to their unique needs, making it difficult for them to form meaningful relationships. Hugley was developed to bridge this gap by offering an intuitive and secure dating experience tailored to their needs.",
-          }
-        ],
-      },   
-      {
-        title: "Solution Statement",
-        content: [
-          {
-            type: "text",
-            text: "Hugley successfully addresses the challenges faced by individuals with intellectual disabilities in the dating world by prioritizing inclusivity, accessibility, and safety.",
-          },
-          {
-            type: "text",
-            text: "Through thoughtful UI/UX design, Hugley fosters a secure and welcoming environment for building meaningful relationships.",
-          }
-        ],
-      },            
-      {
-        title: "Project Screens",
-        content: [
-          {
-            type: "swipper",
-            img_list: [
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833832/Hugley_5_hkaat7.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833833/Hugley_2_drmt4r.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833833/Hugley_3_aicwtg.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833874/Hugley_90_v93hik.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833836/Hugley_4_ietcvt.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833831/Hugley_6_tiorxt.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833831/Hugley_8_jlwhrg.png",
-              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833831/Hugley_7_ul3pfh.png",
-            ],
-          },
-        ],
-      },
-    ],
-    description: [
-      "Pews Pro Church Management Software is an end-to-end software solution that focuses heavily on automating key church activities such as managing church events, coordinating any church payments seamlessly, and keeping a standard database for the congregation.",
-      "It aims at relieving church pastors and administrative workers of the huge burden of keeping track of members, finances, and all other important records.",
-    ],
-  },
+  }, 
   {
     title: "ExoPrime Exosome Website Design",
     status: "Development Stage",
@@ -319,8 +252,77 @@ const projects = [
       "Pews Pro Church Management Software is an end-to-end software solution that focuses heavily on automating key church activities such as managing church events, coordinating any church payments seamlessly, and keeping a standard database for the congregation.",
       "It aims at relieving church pastors and administrative workers of the huge burden of keeping track of members, finances, and all other important records.",
     ],
-  },  
-    {
+  },    
+  {
+    title: "Hugley Dating App Design",
+    status: "Development Stage",
+    image:
+      "https://res.cloudinary.com/delbjwwfy/image/upload/v1738840462/Captureyrrtrt_pnhqyd_mbhtao.png",
+    content: [
+      {
+        title: "Project Overview",
+        content: [
+          {
+            type: "text",
+            text: "Hugley is a unique dating app specifically for individuals with intellectual disabilities, designed to foster meaningful connections in a safe and inclusive environment.",
+          },
+          {
+            type: "text",
+            text: "Whether you're seeking friendship, companionship, or love, Hugley provides a supportive space where users can interact comfortably and authentically.",
+          }                      
+        ],
+      },
+      {
+        title: "Problem Statement",
+        content: [
+          {
+            type: "text",
+            text: "Individuals with intellectual disabilities often encounter barriers when using mainstream dating apps due to complex interfaces, safety concerns, and social stigmas.",
+          },
+          {
+            type: "text",
+            text: "There is a lack of platforms catering to their unique needs, making it difficult for them to form meaningful relationships. Hugley was developed to bridge this gap by offering an intuitive and secure dating experience tailored to their needs.",
+          }
+        ],
+      },   
+      {
+        title: "Solution Statement",
+        content: [
+          {
+            type: "text",
+            text: "Hugley successfully addresses the challenges faced by individuals with intellectual disabilities in the dating world by prioritizing inclusivity, accessibility, and safety.",
+          },
+          {
+            type: "text",
+            text: "Through thoughtful UI/UX design, Hugley fosters a secure and welcoming environment for building meaningful relationships.",
+          }
+        ],
+      },            
+      {
+        title: "Project Screens",
+        content: [
+          {
+            type: "swipper",
+            img_list: [
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833832/Hugley_5_hkaat7.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833833/Hugley_2_drmt4r.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833833/Hugley_3_aicwtg.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833874/Hugley_90_v93hik.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833836/Hugley_4_ietcvt.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833831/Hugley_6_tiorxt.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833831/Hugley_8_jlwhrg.png",
+              "https://res.cloudinary.com/delbjwwfy/image/upload/v1738833831/Hugley_7_ul3pfh.png",
+            ],
+          },
+        ],
+      },
+    ],
+    description: [
+      "Pews Pro Church Management Software is an end-to-end software solution that focuses heavily on automating key church activities such as managing church events, coordinating any church payments seamlessly, and keeping a standard database for the congregation.",
+      "It aims at relieving church pastors and administrative workers of the huge burden of keeping track of members, finances, and all other important records.",
+    ],
+  },
+  {
     title: "Pews Pro Church Management Software",
     status: "Development Stage",
     image:
