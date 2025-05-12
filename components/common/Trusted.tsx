50fe02e648fe65ebd276d974ff8330b79604fe83
Deesuidesign <deesuidesign@gmail.com>
2025-05-12 10:35:02 +0100

    Update Trusted.tsx

--- components/common/Trusted.tsx
+++ components/common/Trusted.tsx
@@ -16,9 +16,9 @@ const companies = [
   { name: "SB Telecoms", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1726056453/y1j3ljfisbquayj4r2z9.svg" },
   { name: "Living", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1726056451/cvbojlojfuqygoqrrc8v.svg" },
   { name: "AirBNB", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1747041277/Exo_Prime_lpnnrp.png" },
-  { name: "Exo Prime", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1727946906/superhost_id_cardrere_fpnadq.png" },
+  { name: "Exo Prime", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1747042451/Exo_Prime_kvqchl.png" },
   { name: "Otiz Technology", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1747041277/Otiz_Logo_iykkaq.png" },
-  { name: "Pews Pro", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1747041276/Pews_Pro_ebctut.png" },
+  { name: "ZKDigimax", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1747042450/digimax_qsdiwi.png" },
   { name: "SG Scoot App", logo: "https://res.cloudinary.com/delbjwwfy/image/upload/v1747041278/SG_d4d71p.png" },
 ];
 
