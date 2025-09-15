b2d319355f26bbbcf98e7d0607201c1523e0bea6
Deesuidesign <deesuidesign@gmail.com>
2025-09-15 10:14:57 +0100

    Update UIUX.tsx

--- components/common/UIUX.tsx
+++ components/common/UIUX.tsx
@@ -22,7 +22,7 @@ const projects = [
     status: "Live",
     image:
       "https://res.cloudinary.com/delbjwwfy/image/upload/v1747049786/Dedata_Hub_UI_ukp2oa.png",
-    link: "https://www.controlaccesshub.com/",
+    link: "https://www.google.com/",
     content: [
       {
         title: "Project Overview",
