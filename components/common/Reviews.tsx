b2dd08fa5f6ad9508e795b299ca905af5e6ca8c7
Deesuidesign <deesuidesign@gmail.com>
2026-09-18 08:07:36 +0100

    Update Reviews.tsx

--- components/common/Reviews.tsx
+++ components/common/Reviews.tsx
@@ -6,7 +6,7 @@ import Avatar from "../../public/avatar.svg";
 const reviews = [
   {
     text: "The design is absolutely stunning! The creativity and attention to detail are impressive. The visuals are striking and really capture the essence of our brand.",
-    name: "Helen David",
+    name: "Helen Davies",
     description: "Product Manager, Flowwave Inc",
   },
   {
